a37be1bd90aecf1ab399ea3af2ce6a73bbc8ac78
Fengyn <2259687396@qq.com>
2018-08-17 17:17:29 +0800

    new: post方法和headMap的获取

--- src/main/java/HttpClientUtil.java
+++ src/main/java/HttpClientUtil.java
@@ -8,7 +8,7 @@ import java.net.*;
 import java.nio.charset.Charset;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.zip.GZIPInputStream;
 
@@ -21,6 +21,9 @@ import java.util.zip.GZIPInputStream;
 public class HttpClientUtil {
     private static final String GET = "get";
     private static final String POST = "post";
+    private static final String HTTPS = "https";
+    private static final String UA = "User-Agent";
+
     /*重试次数*/
     private static final int DEFAULT_RETRY_TIMES = 3;
     /*超时时间*/
@@ -28,66 +31,139 @@ public class HttpClientUtil {
     /*默认字符集*/
     public static final String DEFAULT_CHARSET = "UTF-8";
 
-    public static void main(String[] args) throws Exception {
+    /*电脑UA*/
+    private static List<String> PC_UA_LIST = new ArrayList<String>() {{
+        add("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0");
+        add("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0");
+        add("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0");
+        add("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36");
+        add("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3396.99 Safari/537.36");
+        add("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3396.99 Safari/537.36");
+        add("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3396.99 Safari/537.36");
+        add("Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML); like Gecko); Safari/419.3) Arora/0.6");
+        add("Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5");
+        add("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML); like Gecko) Chrome/19.0.1036.7 Safari/535.20");
+    }};
 
-        String url = "http://www.livemook.com/?s=%E7%94%B7%E8%A3%85&paged=1";
-        String url2 = "http://www.livemook.com/?p=150877";
-        String string = get(url2, null);
-        System.out.println(string);
-
-    }
-
+    private static List<String> PHONE_UA_LIST = new ArrayList<String>() {{
+        add("Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Mobile Safari/537.36");
+        add("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
+        add("Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Mobile Safari/537.36");
+        add("Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Mobile Safari/537.36");
+        add("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
+        add("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/10.0 Mobile/15A372 Safari/604.1");
+        add("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/604.1");
+        add("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1");
+    }};
 
     public static String get(String url, Map<String, String> headMap) throws Exception {
+        AtomicBoolean httpsSign = new AtomicBoolean(false);
+        if (url.contains(HTTPS)) {
+            httpsSign.set(true);
+        }
+        URLConnection connection = null;
         try {
-            URLConnection connection = buildURLConnection(url, GET, headMap);
+            connection = buildURLConnection(url, GET, headMap);
             connection.connect();
-
-            //解析页面字符集(引入了第三方架包)
-            CodepageDetectorProxy cdp = CodepageDetectorProxy.getInstance();
-            cdp.add(JChardetFacade.getInstance());
-            cdp.add(ASCIIDetector.getInstance());
-            cdp.add(UnicodeDetector.getInstance());
-            cdp.add(new ParsingDetector(false));
-            cdp.add(new ByteOrderMarkDetector());
-
-            InputStream inputStream = connection.getInputStream();
-            ByteArrayInputStream bis = null;
-            String encoding = connection.getHeaderField("Content-Encoding");
-            if (encoding != null && encoding.contains("gzip")) {
-                GZIPInputStream gip = new GZIPInputStream(inputStream);
-                inputStream = gip;
-                bis = new ByteArrayInputStream(IOUtils.toByteArray(gip));
-            } else {
-                bis = new ByteArrayInputStream(IOUtils.toByteArray(inputStream));
-            }
-            Charset charset = cdp.detectCodepage(bis, 2147483647);
-
-            BufferedReader reader = null;
-            try {
-                reader = new BufferedReader(new InputStreamReader(bis, charset));
-                StringBuilder builder = new StringBuilder();
-                String line = "";
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line);
-                    builder.append("\r\n");
-                }
-                return builder.toString();
-            } catch (Exception e) {
-                throw new Exception(e);
-            } finally {
-                reader.close();
-                inputStream.close();
-            }
+            return getResponse(connection);
         } catch (Exception e) {
             throw new Exception(e);
+        } finally {
+            if (httpsSign.get()) {
+                ((HttpsURLConnection) connection).disconnect();
+            } else {
+                ((HttpURLConnection) connection).disconnect();
+            }
         }
     }
 
+    public static String get(String url) throws Exception {
+        return get(url, null);
+    }
+
+    public static String post(String url, String body, Map<String, String> headMap) throws Exception {
+        AtomicBoolean httpsSign = new AtomicBoolean(false);
+        if (url.contains(HTTPS)) {
+            httpsSign.set(true);
+        }
+        URLConnection connection = null;
+        try {
+            connection = buildURLConnection(url, body, headMap);
+            if (body != null) {
+                OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), DEFAULT_CHARSET);
+                writer.write(body);
+                writer.flush();
+                writer.close();
+            }
+            return getResponse(connection);
+        } catch (Exception e) {
+            throw new Exception(e);
+        } finally {
+            if (httpsSign.get()) {
+                ((HttpsURLConnection) connection).disconnect();
+            } else {
+                ((HttpURLConnection) connection).disconnect();
+            }
+        }
+    }
+
+    public static String post(String url) throws Exception {
+        return post(url, null, null);
+    }
+
+    private static String getResponse(URLConnection connection) throws Exception {
+        //解析页面字符集(引入了第三方架包)
+        CodepageDetectorProxy cdp = CodepageDetectorProxy.getInstance();
+        cdp.add(JChardetFacade.getInstance());
+        cdp.add(ASCIIDetector.getInstance());
+        cdp.add(UnicodeDetector.getInstance());
+        cdp.add(new ParsingDetector(false));
+        cdp.add(new ByteOrderMarkDetector());
+
+        InputStream inputStream = connection.getInputStream();
+        ByteArrayInputStream bis = null;
+        String encoding = connection.getHeaderField("Content-Encoding");
+        if (encoding != null && encoding.contains("gzip")) {
+            GZIPInputStream gip = new GZIPInputStream(inputStream);
+            inputStream = gip;
+            bis = new ByteArrayInputStream(IOUtils.toByteArray(gip));
+        } else {
+            bis = new ByteArrayInputStream(IOUtils.toByteArray(inputStream));
+        }
+        Charset charset = cdp.detectCodepage(bis, 2147483647);
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(bis, charset));
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append("\r\n");
+            }
+            return builder.toString();
+        } catch (Exception e) {
+            throw new Exception(e);
+        } finally {
+            reader.close();
+            inputStream.close();
+        }
+    }
+
+
+    /**
+     * 构建connection
+     *
+     * @param urlStr
+     * @param method
+     * @param headMap
+     * @return
+     * @throws Exception
+     */
     private static URLConnection buildURLConnection(String urlStr, String method, Map<String, String> headMap) throws Exception {
         URL url = new URL(urlStr);
         AtomicBoolean httpsSign = new AtomicBoolean(false);
-        if (urlStr.contains("https://")) {
+        if (urlStr.contains(HTTPS)) {
             url = new URL(null, urlStr, new Handler());
             httpsSign.set(true);
         }
@@ -99,20 +175,29 @@ public class HttpClientUtil {
         connection.setDoOutput(true);
         connection.setUseCaches(false);
         connection.setReadTimeout(DEFAULT_TIME_OUT);
-        connection.setRequestProperty("Accept-Charset", DEFAULT_CHARSET);
-
+//        connection.setRequestProperty("Accept-Charset", DEFAULT_CHARSET);
         if (headMap != null && !headMap.isEmpty()) {
             for (String property : headMap.keySet()) {
                 connection.setRequestProperty(property, headMap.get(property));
             }
         }
-//        if (httpsSign.get()) {
-//            ((HttpsURLConnection) connection).setRequestMethod(method);
-//        } else {
-//            ((HttpURLConnection) connection).setRequestMethod(method);
-//        }
         return connection;
     }
+
+    public static Map<String,String> getPCUserAgentHeadMap(){
+        Map<String, String> map = new HashMap<String, String>();
+        Collections.shuffle(PC_UA_LIST);
+        map.put(UA,PC_UA_LIST.get(0));
+        return map;
+    }
+
+    public static Map<String,String> getPhoneUAHeadMap(){
+        Map<String, String> map = new HashMap<String, String>();
+        Collections.shuffle(PHONE_UA_LIST);
+        map.put(UA,PHONE_UA_LIST.get(0));
+        return map;
+    }
+
 
 
     //----------------------------------------------------------------------------------------
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,24 @@
+import java.net.URLEncoder;
+import java.util.Map;
+
 public class Test {
+
     public static void main(String[] args) throws Exception {
 
-        String url = "http://www.livemook.com/?s=%E7%94%B7%E8%A3%85&paged=1";
-        String string = HttpClientUtil.get(url, null);
-        System.out.println(string);
+//        String url = "http://www.livemook.com/?s=%E7%94%B7%E8%A3%85&paged=1";
+//        String string = HttpClientUtil.get(url);
+//        System.out.println(string);
+
+        String url = "http://bbs.xbiao.com/search.php?mod=forum";
+        String body = "formhash=0cf6abff&srchtxt=%E5%8A%B3%E5%8A%9B%E5%A3%AB&searchsubmit=yes";
+//        Map<String, String> headMap = UserAgent.getUserAgentHeadMap();
+
+        String post = HttpClientUtil.post(url, body, null);
+        System.out.println(post);
+
+//        String encode1 = URLEncoder.encode(string, "UTF-8");
+//        String encode2 = URLEncoder.encode(string, "GBK");
+//        System.out.println(encode1);
+//        System.out.println(encode2);
     }
 }
